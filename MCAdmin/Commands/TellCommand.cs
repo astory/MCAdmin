ea4532dccc2226c8e8a58a46ec3aa3a57165d5df
supercabbage <supercabbage@bff262af-d8e0-4d19-9a35-3eece30b5779>
2010-10-04 01:58:21 +0000

    Tell command. Whispers to target player. Messages are logged.
    git-svn-id: http://svn.doridian.de/mcadmin@85 bff262af-d8e0-4d19-9a35-3eece30b5779

---
+++ MCAdmin/Commands/TellCommand.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace MCAdmin.Commands
+{
+    class TellCommand : Command
+    {
+        public override void Run(Player ply, string[] cmdparts)
+        {
+            try
+            {
+                Player ply2 = Program.minecraftFirewall.FindPlayer(cmdparts[1]);
+                if (ply2 == null) { ply.SendDirectedMessage("Sorry, target could not be found!"); return; }
+
+                var message = new StringBuilder();
+
+                for (var i = 2; i < cmdparts.Length; i++)
+                    message.Append(cmdparts[i] + " ");
+                message.Remove(message.Length - 1, 1);
+
+                Program.AddRTLine(Color.Black, ply.name + " whispered to " + ply2.name + ": " + message + "\n", true );
+                ply.SendDirectedMessage("You whisper to " + ply2.name + ".");
+                ply2.SendDirectedMessage(ply.name + " whispers: " + message );
+            }
+            catch (Exception ex)
+            {
+                Program.SendLogMsg(ex.ToString());
+            }
+
+        }
+
+        public override int reqlevel { get { return 1; } }
+
+        public override string Help { get { return "Sends a private message to specified player."; } }
+        public override string Usage { get { return "<playername>"; } }
+
+    }
+}
