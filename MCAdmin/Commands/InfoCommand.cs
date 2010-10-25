f73b679f3ef77392211273eb5fd7a0ec70be9db0
mitchy <mitchy@bff262af-d8e0-4d19-9a35-3eece30b5779>
2010-10-25 06:06:07 +0000

    -facepalm-  Forgot to add InfoCommand to the list. Thanks Dori <3
    git-svn-id: http://svn.doridian.de/mcadmin@93 bff262af-d8e0-4d19-9a35-3eece30b5779

---
+++ MCAdmin/Commands/InfoCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MCAdmin.Commands
+{
+    class InfoCommand : Command
+    {
+        public override void Run(Player ply, string[] cmdparts)
+        {
+            ply.SendDirectedMessage("§9" + Program.GetServerProperty("server-name", "MCAdmin Server"));
+            ply.SendDirectedMessage("§eServer Address: §f" + Program.externalIP + ":" + Program.GetServerProperty("server-port-real", "25565"));
+            ply.ReadMsgFile("info");
+        }
+
+        public override int reqlevel { get { return 0; } }
+
+        public override string Help { get { return "Displays Information about the server aswell as info from the info file"; } }
+        public override string Usage { get { return ""; } }
+    }
+}
