eb28974ff2edb14070d4fbbb135f5bd0d216752d
mitchy <mitchy@bff262af-d8e0-4d19-9a35-3eece30b5779>
2010-10-28 04:54:32 +0000

    Made it Fancier :3
    git-svn-id: http://svn.doridian.de/mcadmin@108 bff262af-d8e0-4d19-9a35-3eece30b5779

--- MCAdmin/Commands/VersionCommand.cs
+++ MCAdmin/Commands/VersionCommand.cs
@@ -9,14 +9,18 @@ namespace MCAdmin.Commands
     {
         public override void Run(Player ply, string[] cmdparts)
         {
-            ply.SendDirectedMessage("MCAdmin (c) 2010 by Doridian the blue haired fox :3");
+            ply.SendDirectedMessage("§9MCAdmin (c) 2010");
+            ply.SendDirectedMessage("§eBy Doridian the Blue Haired Fox");
+            ply.SendDirectedMessage("");
+            ply.SendDirectedMessage("§eYou are not allowed to edit MCAdmin in any way, unless you have permission");
+            ply.SendDirectedMessage("§efrom Doridian. MCAdmin is not Open Source.");
         }
 
         public override int minlevel { get { return reqlevel; } set { } }
 
         public override int reqlevel { get { return 0; } }
 
-        public override string Help { get { return "Gives name and author of this :3"; } }
+        public override string Help { get { return "Gives Name and Author of this :3"; } }
         public override string Usage { get { return ""; } }
     }
 }
