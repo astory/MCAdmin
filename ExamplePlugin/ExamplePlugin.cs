2c16bdd7579cfe42cede6f9cca7d0583c8e86df6
admin <admin@bff262af-d8e0-4d19-9a35-3eece30b5779>
2010-10-29 17:59:45 +0000

    Added example plugin.
    git-svn-id: http://svn.doridian.de/mcadmin@117 bff262af-d8e0-4d19-9a35-3eece30b5779

---
+++ ExamplePlugin/ExamplePlugin.cs
@@ -0,0 +1,15 @@
+﻿using MCAdmin.Commands;
+namespace MCAdmin.Plugins
+{
+    class ExamplePlugin : Plugin
+    {
+        public override void Load()
+        {
+            Program.AddRTLine(System.Drawing.Color.Black, "Loaded ExamplePlugin\r\n", true);
+        }
+        public override void Unload()
+        {
+            Program.AddRTLine(System.Drawing.Color.Black, "Unloaded ExamplePlugin\r\n", true);
+        }
+    }
+}
