b11df14c42149fa8c4decd5784b4ac4928097538
mitchy <mitchy@bff262af-d8e0-4d19-9a35-3eece30b5779>
2010-11-03 06:17:27 +0000

    Fixed/Updated !credits
    git-svn-id: http://svn.doridian.de/mcadmin@144 bff262af-d8e0-4d19-9a35-3eece30b5779

--- MCAdmin/Commands/CreditsCommand.cs
+++ MCAdmin/Commands/CreditsCommand.cs
@@ -13,17 +13,17 @@ namespace MCAdmin.Commands
         public override void Run(Player ply, string[] cmdparts)
         {
             ply.SendDirectedMessage("§9MCAdmin (c) 2010");
-            ply.SendDirectedMessage("§a- §bDoridian: §fFounder & Main Blue Haired Fox Developer ");
-            ply.SendDirectedMessage("§a- §bToxicated: Combine Soldier Developer");
+            ply.SendDirectedMessage("§a- §bDoridian: §fFounder & Main Blue Haired Fox Developer");
+            ply.SendDirectedMessage("§a- §bToxicated: §fCombine Soldier Developer");
             ply.SendDirectedMessage("");
-            ply.SendDirectedMessage("§eMCAdmin is released under the terms of the §cBSD License");
+            ply.SendDirectedMessage("§eMCAdmin is released under the §cBSD License§e.");
         }
 
         public override int minlevel { get { return reqlevel; } set { } }
 
         public override int reqlevel { get { return 0; } }
 
-        public override string Help { get { return "Gives the Credits & Developer for MCAdmin :3"; } }
+        public override string Help { get { return "Gives the Credits & Developers for MCAdmin :3"; } }
         public override string Usage { get { return ""; } }
     }
 }
